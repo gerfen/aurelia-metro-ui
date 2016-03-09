d7caa5717cc458bf53e28ee454e5331587f07380
Bojan Petkovic <b_petkovic@hotmail.com>
2016-03-09 13:40:55 +0200

    fixing build

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var paths = require('../paths');
-
+var merge = require('merge2');  // Require separate installation 
+var ts = require('gulp-typescript');
 
 gulp.task('build-html', function () {
   return gulp.src(paths.html)
@@ -11,10 +12,27 @@ gulp.task('build-html', function () {
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
+
+gulp.task('compile-amd', function() {
+	var tsResult = gulp.src('src/**/*.ts')
+		.pipe(ts({
+			declaration: true,
+			noExternalResolve: true,
+            experimentalDecorators :true,
+            allowJs :true,
+            outFile : paths.output + 'amd/aurelia-metro-ui.js'
+		}));
+ 
+	return merge([
+		tsResult.dts.pipe(gulp.dest(paths.output + 'amd')),
+		tsResult.js.pipe(gulp.dest(paths.output + 'amd'))
+	]);
+});
+
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
+    'compile-amd',
     [/*'build-es6', 'build-commonjs', 'build-amd', 'build-system',*/ 'build-html'],
-    callback
-  );
+    callback);
 });
--- src/aurelia-metro-ui@1.0.0/metro-ui-appbar.ts
+++ src/aurelia-metro-ui@1.0.0/metro-ui-appbar.ts
@@ -1,4 +1,3 @@
-import { IDictionaryPair , Dictionary } from "util/collections/dictionary";
 import { inject, bindable, customElement, bindingMode } from "aurelia-framework";
 
 @inject(Element)
