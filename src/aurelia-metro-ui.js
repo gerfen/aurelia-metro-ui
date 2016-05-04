357ce3e79ebf8b6374c064752ef452a048701237
Bojan Petkovic <BojanP@avt-inc.com>
2016-05-04 13:43:29 +0300

    Removed js files from src

--- src/aurelia-metro-ui.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", './services/muDialogService', './services/muNotifyService'], function (require, exports, muDialogService_1, muNotifyService_1) {
-    "use strict";
-    function configure(config, callback) {
-        config.globalResources('./resources/mu-button');
-    }
-    exports.configure = configure;
-    exports.MuDialogService = muDialogService_1.MuDialogService;
-    exports.MuNotifyService = muNotifyService_1.MuNotifyService;
-});
